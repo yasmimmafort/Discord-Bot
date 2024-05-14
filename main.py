4605dd0af21e08a3089c29620ac908af96698474
Yasmim Mafort <yasmimmafort@gmail.com>
2024-05-14 19:53:40 -0300

    Add files via upload
    Add banco de dados local em um arquivo txt

--- main.py
+++ main.py
@@ -1,20 +1,38 @@
 import discord
 from discord.ext import commands
-
+import os
 
 intents = discord.Intents.default()
 intents.message_content = True
 
+FILENAME = "tarefas.txt"
 
-client = commands.Bot(command_prefix='!', intents=intents)
+def carregar_lista():
+    if os.path.exists(FILENAME):
+        with open(FILENAME, "r", encoding="utf-8") as f:
+            linhas = f.readlines()  
+            tarefas = [linha.strip() for linha in linhas]
+            return tarefas
+    else:
+        return []
 
 
-lista = []
+def salvar_lista(lista):
+    with open(FILENAME, "w", encoding="utf-8") as f:
+        for tarefa in lista:
+            f.write(f"{tarefa}\n")
+
+
+
+client = commands.Bot(command_prefix='!', intents=intents) 
+
+
+lista = carregar_lista()
 
 
 @client.event
 async def on_message(message):
-    if message.channel.id != id da sala:
+    if message.channel.id != #id da sala de comandos:
         return  
 
     await client.process_commands(message)  
@@ -24,21 +42,19 @@ async def on_message(message):
 async def on_ready():
     print(f'We have logged in as {client.user}')
 
-
 @client.group(name='tarefas')
 async def tarefas(ctx):
     if ctx.invoked_subcommand is None:
         await ctx.send("Use os subcomandos: \nadicionar - Para adicionar uma tarefa a lista. \nlistar - Para listar todas as tarefas incluidas na lista. \nremover - Para remover uma tarefa da lista ")
 
-
 @tarefas.command(name='adicionar')
 async def adicionar_tarefa(ctx, *, tarefa: str = None):
     if tarefa:
         lista.append(tarefa)
+        salvar_lista(lista)
         await ctx.send(f'Tarefa "{tarefa}" adicionada à lista.')
     else:
         await ctx.send("Por favor, forneça uma tarefa para adicionar.")
-
 
 @tarefas.command(name='listar')
 async def listar_tarefas(ctx):
@@ -48,25 +64,24 @@ async def listar_tarefas(ctx):
     else:
         await ctx.send("A lista de tarefas está vazia.")
 
-
 @tarefas.command(name='remover')
 async def remover_tarefa(ctx, indice: int):
     if 0 < indice <= len(lista):
-        tarefa_removida = lista.pop(indice - 1) 
+        tarefa_removida = lista.pop(indice - 1)  
+        salvar_lista(lista)
         await ctx.send(f'Tarefa "{tarefa_removida}" foi removida.')
     else:
         await ctx.send(f"Item da lista invalido. Forneça um número entre 1 e {len(lista)}.")
 
-
 @client.command(name='juntar')        
 async def juntar_strings(ctx, *args):
     if args:
-        string_args = [str(arg) for arg in args]
-        resultado = " ".join(args)  
+        string_args = [str(arg) for arg in args] 
+        resultado = " ".join(string_args) 
         await ctx.send(resultado)
     else:
         await ctx.send("Forneça pelo menos uma string para juntar.")
 
         
 
-client.run('seu token')
+client.run('Token do seu bot')
